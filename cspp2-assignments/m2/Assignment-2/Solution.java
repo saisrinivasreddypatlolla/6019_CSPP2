7650a056fbb608c78f1ed3bb305c3bcdc05a9136
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-28 15:25:28 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-2/Solution.java
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -6,20 +6,20 @@ public class Solution {
 	Do not modify this main function.
 	*/
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner scan=new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
 		rootsOfQuadraticEquation(a, b, c);
 	}
-	static void rootsOfQuadraticEquation(int a, int b, int c){
-		double root = Math.sqrt((b*b) - (4*a*c));
-		double numerator1 = -b+root;
-		double numerator2 = -b-root;
-		double denominator = 2*a;
-		double value1 = numerator1/denominator;
-		double value2 = numerator2/denominator;
-		System.out.println(value1+" "+value2);
+	static void rootsOfQuadraticEquation(int a, int b, int c) {
+		double root = Math.sqrt((b * b) - (4 * a * c));
+		double numerator1 = -b + root;
+		double numerator2 = -b - root;
+		double denominator = 2 * a;
+		double value1 = numerator1 / denominator;
+		double value2 = numerator2 / denominator;
+		System.out.println(value1 + " " + value2);
 
 	}
 	/*

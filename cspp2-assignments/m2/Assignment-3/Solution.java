299d2f826e4aed22a811111edb4969c93c7cea6e
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-28 15:12:49 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -21,13 +21,13 @@ public class Solution {
         }
         if(exponent >= 1){
             if(exponent > 1)
-                base *= power(base,exponent-1);
-                System.out.println(base+" "+exponent);
+                base *= power(base, exponent-1);
+                System.out.println(base + " " + exponent);
             base = base*1;
         }
         else if(exponent == 0) return 1;
         if(flag == true)
-            return -base;
+            return - base;
         else
             return base;
     }

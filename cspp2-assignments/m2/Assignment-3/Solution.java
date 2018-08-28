93adc6582d553b682cd9fdf86fdd1b7a70f8a360
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-28 15:21:16 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     Do not modify this main function.
     */
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result = power(base, exponent);

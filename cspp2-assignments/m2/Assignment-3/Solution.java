37e82070bc03475e584c26eaab54c9e420c96434
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-28 15:15:59 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -12,26 +12,26 @@ public class Solution {
         long result = power(base,exponent);
         System.out.println(result);
     }
-    static long power(int base, int exponent){
+    static long power(int base, int exponent) {
         boolean flag = false;
         long number = 0;
-        if(base < 0){
+        if(base < 0) {
             flag = true;
             base = Math.abs(base);
         }
-        if (exponent >= 1){
+        if (exponent >= 1) {
             if (exponent > 1)
                 base *= power(base, exponent-1);
                 System.out.println(base + " " + exponent);
             base = base*1;
         }
-        else if (exponent == 0){
+        else if (exponent == 0) {
             return 1;
         }
-        if (flag == true){
+        if (flag == true) {
             return - base;
         }
-        else{
+        else {
             return base;
         }
     }

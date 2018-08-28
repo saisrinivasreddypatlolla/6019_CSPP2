5584aedfac9ef3382e8d88de782348baf4ae634c
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-28 14:49:34 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -2,37 +2,36 @@ import java.util.Scanner;
 import java.math.*;
 
 public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+    /*
+    Do not modify this main function.
+    */
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result=power(base,exponent);
         System.out.println(result);
-	}
-	static long power(int base, int exponent){
-		boolean flag = false;
-		long number = 0;
-		if(base < 0){
-			flag = true;
-			base = Math.abs(base);
-		}
-		if(exponent >= 1){
-			if(exponent > 1){
-				base *= power(base,exponent-1);
-			}
-			base = base*1;
-		}
-		else if(exponent == 0)
-			return 1;
-		if(flag == true)
-			return -base;
-		else
-			return base;
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
+    }
+    static long power(int base, int exponent){
+        boolean flag = false;
+        long number = 0;
+        if(base < 0){
+            flag = true;
+            base = Math.abs(base);
+        }
+        if(exponent >= 1){
+            if(exponent > 1)
+                base *= power(base,exponent-1);
+            base = base*1;
+        }
+        else if(exponent == 0)
+            return 1;
+        if(flag == true)
+            return -base;
+        else
+            return base;
+    }
+    /*
+    Need to write the power function and print the output.
+    */
 }

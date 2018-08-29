f1dac488aab8e3dc68708602818264c6cd1fd6a2
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-29 13:49:54 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
  * Program for gcd values
  */
 public final class Solution {
+    private static int gcd = 1;
     /**.
      * class for printing gcd values
      */
-    static int gcd = 1;
     private Solution() {
         /**.
          * private constructor.
@@ -24,23 +24,25 @@ public final class Solution {
         Scanner s = new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        System.out.println(gcd(n1,n2));
+        System.out.println(gcd(n1, n2));
     }
     /**
      * checks the condition and returns greatest common
      * factor of given two numbers.
      *
-     * @param      number1  The number 1
-     * @param      number2  The number 2
+     * @param      n1  The number 1
+     * @param      n2  The number 2
      *
      * @return     integer
      */
-    static int gcd(int number1, int number2) {
+    static int gcd(final int n1, final int n2) {
         int temp = 0;
+        int number1 = n1;
+        int number2 = n2;
         if (number1 > number2) {
             temp = number1;
             number1 = number2;
-            number2 =temp;
+            number2 = temp;
         }
         for (int i = 1; i <= number2; i++) {
             if ((number1 % i == 0) && (number2 % i == 0)) {

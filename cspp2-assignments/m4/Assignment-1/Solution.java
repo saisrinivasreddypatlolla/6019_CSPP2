f62f64aa795458e507ff46b261fd59c93d3aa89d
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-30 12:21:45 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -1,35 +1,36 @@
 
-import java.util.*;
+import java.util.Scanner;
 /**.
  * { item_description }
  */
 public final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
-		/**.
-		 * { item_description }
-		 */
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
 
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		int[] arr = new int[n];
-		int i = 0;
-		for (; i < n; i++)
-			arr[i] = Integer.parseInt(sc.nextLine());
-		for (i = 0; i < n - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
-				arr[i + 1] = arr[i];
-			}
-		}
-		System.out.println(arr[i]);
-	}
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int[] arr = new int[n];
+        int i = 0;
+        for (; i < n; i++){
+            arr[i] = Integer.parseInt(sc.nextLine());
+        }
+        for (i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                arr[i + 1] = arr[i];
+            }
+        }
+        System.out.println(arr[i]);
+    }
 }

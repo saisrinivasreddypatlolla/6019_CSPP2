c7d75ade1b82b228353965e4e89b17152d226f08
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-30 15:41:45 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -0,0 +1,55 @@
+import java.util.*;
+/**.
+ * This program is to print additon of two matrices.
+ */
+public final class Solution {
+    /**.
+     * This class is to print additon of two matrices.
+     */
+    private Solution(){
+        /**.
+         * private constructor.
+         */
+    }
+    /**
+     * main function to print additon of two matrices.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        int row, column;
+        String rowString;
+        int[][] matrix1 = matrix();
+        int[][] matrix2 = matrix();
+        int[][] sum = new int[matrix1.length][matrix1[0].length];
+        for (row = 0; row < matrix1.length; row++) {
+            for (column = 0; column < matrix1[0].length; column++) {
+                sum[row][column] = matrix1[row][column] + matrix2[row][column];
+            }
+        }
+        for (row = 0; row < matrix1.length; row++) {
+            rowString = "";
+            for (column = 0; column < matrix1[0].length - 1; column++) {
+                rowString += sum[row][column] + " ";
+            } System.out.println(rowString + sum[row][column]);
+        }
+    }
+    /**
+     * this functon return the matrix values to other matrix.
+     *
+     * @return     matrix
+     */
+    public static int[][] matrix() {
+        Scanner scan = new Scanner(System.in);
+        int rows = scan.nextInt();
+        int columns = scan.nextInt();
+        int[][] matrix = new int[rows][columns];
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                matrix[row][column] = scan.nextInt();
+            }
+        }
+        return matrix;
+
+    }
+}

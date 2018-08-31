db7d50caf74f32a3dbbe9ad8693391aeff163d37
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-31 16:38:53 +0530

    concatination of two strings

---
+++ cspp2-practice/m4/Concatinate.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Concatinate{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String name = scan.nextLine();
+		System.out.println("Hello "+name+"!");
+	}
+}

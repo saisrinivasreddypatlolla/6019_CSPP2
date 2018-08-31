93da132da86ec40c4a6323aa708b32b10957550c
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-31 16:36:41 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -4,39 +4,39 @@ import java.util.Scanner;
  * This program to print reverse of given string.
  */
 public final class Solution {
-	/**.
-	 * This class to print reverse of given string.
-	 */
-	private Solution() {
-		/**.
-		 * private constructor.
-		 */
-	}
-	/**.
-	 * This main function to print reverse of given string.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);
-		System.out.println(reverse);
+    /**.
+     * This class to print reverse of given string.
+     */
+    private Solution() {
+        /**.
+         * private constructor.
+         */
+    }
+    /**.
+     * This main function to print reverse of given string.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
 
-	}
-	/**.
-	 * This function reverse the given string and return that string.
-	 *
-	 * @param      string  The string
-	 *
-	 * @return     returns string type.
-	 */
-	static String reverseString(final String string) {
-		String reverseString = "";
-		for (int index = 0; index < string.length(); index++) {
-			reverseString = string.charAt(index) + reverseString;
-		}
-		return reverseString;
-	}
+    }
+    /**.
+     * This function reverse the given string and return that string.
+     *
+     * @param      string  The string
+     *
+     * @return     returns string type.
+     */
+    static String reverseString(final String string) {
+        String reverseString = "";
+        for (int index = 0; index < string.length(); index++) {
+            reverseString = string.charAt(index) + reverseString;
+        }
+        return reverseString;
+    }
 
 }

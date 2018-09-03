fa31c2179a7a1fec7d7574c618ccb7974c8f7bd3
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-03 18:05:58 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -3,54 +3,55 @@ import java.util.Scanner;
  * Class for input validator.
  */
 class InputValidator {
-	/**.
-	 * { var_description }
-	 */
-	private static String value;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      input  The input
-	 */
-	InputValidator(final String input) {
-		this.value = input;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public boolean validateData() {
-		if (value.length() >= 6) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    /**.
+     * { var_description }
+     */
+    final int length = 6;
+    private static String value;
+    /**.
+     * Constructs the object.
+     *
+     * @param      input  The input
+     */
+    InputValidator(final String input) {
+        this.value = input;
+    }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean validateData() {
+        if (value.length() >= length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 /**.
  * Class for solution.
  */
 public final class Solution {
-	/**.
-	 * { item_description }
-	 */
-	private Solution() {
-		/**.
-		 * { item_description }
-		 */
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		String input = s.next();
-		InputValidator i = new InputValidator(input);
-		System.out.println(i.validateData());
+    /**.
+     * { item_description }
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
-	}
+    }
 
 }

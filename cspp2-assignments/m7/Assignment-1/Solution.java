c45b7f8b94c4dc7804a7042e2a714764a27b6b4d
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-03 17:55:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -1,9 +1,25 @@
 import java.util.*;
+/**.
+ * Class for input validator.
+ */
 class InputValidator {
+	/**.
+	 * { var_description }
+	 */
 	static String value;
+	/**.
+	 * Constructs the object.
+	 *
+	 * @param      input  The input
+	 */
 	public InputValidator(String input) {
 		this.value = input;
 	}
+	/**.
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public boolean validateData() {
 		if (value.length() >= 6) {
 			return true;
@@ -12,6 +28,9 @@ class InputValidator {
 		}
 	}
 }
+/**.
+ * Class for solution.
+ */
 public class Solution {
 	public static void main(String args[]) {
 		Scanner s = new Scanner(System.in);

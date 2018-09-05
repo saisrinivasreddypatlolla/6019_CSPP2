04241efe0d09cc2ead28f197a93349b8f61372c2
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-05 10:09:12 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m9/Assignment-1/Fibonacci.java
+++ cspp2-assignments/m9/Assignment-1/Fibonacci.java
@@ -27,14 +27,22 @@ public class Fibonacci
      */
     public static List fib(int n) {
         // todo - complete this method
-        return null;
+        List l = new List();
+        int a = 0,b = 1,c;
+        l.add(a);
+        l.add(b);
+        for(int i = 2;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            l.add(b);
+        }
+        return l;
     }
 
 	public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         System.out.println(fib(n));
-        List l = new List();
-        l.toString();
     }
 }

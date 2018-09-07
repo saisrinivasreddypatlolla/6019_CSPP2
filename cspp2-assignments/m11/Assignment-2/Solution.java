32ff40a4382ec80d2f1fd6e9b32fa4bcae899694
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-07 17:44:26 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-2/Solution.java
+++ cspp2-assignments/m11/Assignment-2/Solution.java
@@ -48,7 +48,7 @@ class Student {
      * @return     { description_of_the_return_value }
      */
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return 0;
     }
     /**.
@@ -132,7 +132,9 @@ public class Solution {
                 }
                 break;
             case "subList":
-                if (tokens.length != 2) break;
+                if (tokens.length != 2) {
+                    break;
+                }
                 String[] arrstring3 = tokens[1].split(",");
                 List object = l.subList(Integer.parseInt(arrstring3[0]),
                                         Integer.parseInt(arrstring3[1]));
@@ -230,7 +232,9 @@ public class Solution {
                 }
                 break;
             case "subList":
-                if (tokens.length != 2) { break;}
+                if (tokens.length != 2) {
+                    break;
+                }
                 String[] arrstring3 = tokens[1].split(",");
                 List object = l.subList(Integer.parseInt(arrstring3[0]),
                                         Integer.parseInt(arrstring3[1]));
@@ -329,7 +333,9 @@ public class Solution {
                 }
                 break;
             case "subList":
-                if (tokens.length != 2) { break;}
+                if (tokens.length != 2) {
+                    break;
+                }
                 String[] arrstring3 = tokens[1].split(",");
                 List object = l.subList(Integer.parseInt(arrstring3[0]),
                                         Integer.parseInt(arrstring3[1]));
@@ -416,7 +422,9 @@ public class Solution {
                 }
                 break;
             case "subList":
-                if (tokens.length != 2) { break;}
+                if (tokens.length != 2) {
+                    break;
+                }
                 String[] arrstring3 = tokens[1].split(",");
                 List object = l.subList(Integer.parseInt(arrstring3[0]),
                                         Integer.parseInt(arrstring3[1]));
@@ -515,7 +523,9 @@ public class Solution {
                 }
                 break;
             case "subList":
-                if (tokens.length != 2) { break;}
+                if (tokens.length != 2) {
+                    break;
+                }
                 String[] arrstring3 = tokens[1].split(",");
                 List object = l.subList(Integer.parseInt(
                                             arrstring3[0]),

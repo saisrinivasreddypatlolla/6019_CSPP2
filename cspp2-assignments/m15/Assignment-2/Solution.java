da5fac721e2feb9e22d69f2ef51a54260c709366
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-11 20:47:17 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-2/Solution.java
+++ cspp2-assignments/m15/Assignment-2/Solution.java
@@ -110,9 +110,9 @@ class Set {
 		}
 		return set[size - 1];
 	}
-	public int[] subSet(final int start, final int end)throws Exception {
+	public int[] subSet(final int start, final int end)throws InvalidSubsetSelectionException {
 		if (start > end) {
-			throw new Exception("Invalid Arguments to Subset Exception");
+			throw new InvalidSubsetSelectionException("Invalid Arguments to Subset Exception");
 		}
 		if (size == 0) {
 			int[] subSet = new int[0];

9f998919fb3a14eb11bc15d3676d86154b80b894
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-17 10:04:44 +0530

    "check_style_score -> 7 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Quiz.java
+++ cspp2-assignments/m19/Assignment-1/Quiz.java
@@ -53,7 +53,9 @@ class Quiz{
 	public void print(int ques){
 		for(int i = 0;i<ques;i++){
 			System.out.println(questions[i]+"("+marks[i]+")");
-			System.out.println(options[i][0]+"\t"+options[i][1]+"\t"+options[i][2]+"\t"+options[i][3]);
+			for(int j=0;j<options[i].length-1;j++){
+				System.out.print(options[i][j]+"\t");
+			}System.out.print(options[i][options[i].length-1]);
 			System.out.println();
 
 		}

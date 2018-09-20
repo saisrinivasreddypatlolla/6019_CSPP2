124364624fb9a2f9e77f5e77e2e3ee033e9dd299
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-21 00:39:26 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -31,28 +31,15 @@ public final class Solution {
 			}
 			System.out.println();
 			for (File files1 : files) {
-				// File dir = new File("")
 				for (File files2 : files) {
 					Distance map1 = new Distance(files1);
-					// System.out.println(map1.getNumOfLines() + " lines  ");
 					double sum1 = map1.ecludian();
-					// E:/CSPP2/6019_CSPP2/cspp2-assignments/m23/HW-4 Document Distance/src/verne.txt
-					// System.out.println(map1.getNumOfWords() + " words  ");
-					// System.out.println(map1.getNumOfDistinctWords() + " distinct words");
-					// System.out.println(map1.norm() + " Euclidean norm\n");
-					// map1.file();
 					HashMap<String, Integer> value1 = map1.name;
-					// System.out.println(map1.name);
 					List<Integer> values = new ArrayList<Integer>(map1.name.values());
 					List<String> keys = new ArrayList<String>(map1.name.keySet());
 					Distance map2 = new Distance(files2);
-					// System.out.println(map2.getNumOfLines() + " lines  ");
-					// System.out.println(map2.getNumOfWords() + " words  ");
-					// System.out.println(values);
 					double sum2 = map2.ecludian();
 					HashMap<String, Integer> value2 = map2.name;
-					// System.out.println(map2.getNumOfDistinctWords() + " distinct words");
-					// System.out.println(map2.norm() + " Euclidean norm\n");
 					List<Integer> values3 = new ArrayList<Integer>(map2.name.values());
 					double sum = 0;
 					for (String i : value1.keySet()) {
@@ -60,48 +47,10 @@ public final class Solution {
 							sum += value1.get(i) * value2.get(i);
 						}
 					}
-					// System.out.println(values3);
-					// int size = 0;
-					// if(values3.size()<=values.size()){
-					// 	size = values3.size();
-					// }else{
-					// 	size = values.size();
-					// }
-					// for (int i = 0; i < size-1; i++) {
-					// 	// sum += value1.get(each) * value2.get(each);
-					// 	sum += values.get(i) * values3.get(i);
-					// 	// System.out.println(sum);
-					// }
-					// System.out.println(sum+" sum1 "+sum1+" sum2 "+sum2);
-					// System.out.println(map2.innerProduct(map1.getMap()) + " inner product");
-					// System.out.println(map1.angle(map2.getMap()) + " angle");
-					// double temp;
-					// if (sum1 > sum2) {
-					// 	temp = sum1;
-					// 	sum1 = sum2;
-					// 	sum2 = temp;
-					// }
 					double per = (sum / (Math.sqrt(sum1) * Math.sqrt(sum2))) * 100;
 					System.out.println(Math.round(per));
 				}
 			}
-
-
-			/*                   EXPECTED OUTPUT
-			1057 lines
-			8943 words
-			2154 distinct words
-			886.5624625484659 Euclidean norm
-
-			15996 lines
-			182357 words
-			8539 distinct words
-			22206.41589721313 Euclidean norm
-
-			1.7962605E7 inner product
-			0.4217057138495239 angle
-			*/
-
 		} catch (Exception e) {
 			System.out.println("empty directory");
 		}

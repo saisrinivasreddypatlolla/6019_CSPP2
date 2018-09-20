bb50a2b2efb237cb1770887b2f6abe206a223ab3
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-09-21 00:43:16 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -31,7 +31,7 @@ public final class Solution {
                 return;
             }
             for (File i : files) {
-                System.out.print("\t" + i );
+                System.out.print("\t" + i);
             }
             System.out.println();
             for (File files1 : files) {
@@ -39,19 +39,24 @@ public final class Solution {
                     Distance map1 = new Distance(files1);
                     double sum1 = map1.ecludian();
                     HashMap<String, Integer> value1 = map1.name;
-                    List<Integer> values = new ArrayList<Integer>(map1.name.values());
-                    List<String> keys = new ArrayList<String>(map1.name.keySet());
+                    List<Integer> values = new ArrayList<Integer>(
+                        map1.name.values());
+                    List<String> keys = new ArrayList<String>(
+                        map1.name.keySet());
                     Distance map2 = new Distance(files2);
                     double sum2 = map2.ecludian();
                     HashMap<String, Integer> value2 = map2.name;
-                    List<Integer> values3 = new ArrayList<Integer>(map2.name.values());
+                    List<Integer> values3 = new ArrayList<Integer>(
+                        map2.name.values());
                     double sum = 0;
                     for (String i : value1.keySet()) {
                         if (value2.containsKey(i)) {
                             sum += value1.get(i) * value2.get(i);
                         }
                     }
-                    double per = (sum / (Math.sqrt(sum1) * Math.sqrt(sum2))) * HUNDRED;
+                    double per = (sum / (Math.sqrt(
+                                             sum1) * Math.sqrt(
+                                             sum2))) * HUNDRED;
                     System.out.println(Math.round(per));
                 }
             }
